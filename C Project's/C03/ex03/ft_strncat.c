78706d7604c549294d85d212cee98d348bdf6512
Proffesor001 <144912810+Proffesor001@users.noreply.github.com>
2024-08-22 17:10:07 +0200

    Update ft_strncat.c

--- C Project's/C03/ex03/ft_strncat.c
+++ C Project's/C03/ex03/ft_strncat.c
@@ -21,7 +21,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 	j = 0;
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0' && j < 2)
+	while (src[j] != '\0' && j < nb)
 	{
 		dest[i] = src[j];
 		i++;
